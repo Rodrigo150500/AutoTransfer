23bfdabb1006fc3e939304abcc18bdc51398d6d5
Rodrigo Takara <rodrigo.takara1505@gmail.com>
2024-11-01 12:30:43 -0300

    🟡 Main.py - Organizando código

--- Main.py
+++ Main.py
@@ -1,7 +1,4 @@
-from os.path import isdir
-
-from pandas import read_excel
-from pandas.io.clipboard import paste
+from pandas import read_excel, isna
 
 from isRunningWhere import acharCaminhoExecutado
 from shutil import copy, copytree
@@ -12,45 +9,76 @@ file = read_excel(acharCaminhoExecutado('./Excel/AutoTransfer.xlsx'))
 listaOrigem = file['Origem']
 listaDestino = file['Destino']
 
-def autoTransfer():
 
-    if(len(listaDestino) != len(listaOrigem)):
-        input("A quantidade de Origem está diferente da quantidade de Destino\nVerifique se cada origem tem seu destino")
-        return
+def autoTransfer():
+    print("="*30)
+    print("INICIANDO AUTO TRANSFER")
+    print("="*30)
+
+    linha = 2
     for i in range(len(listaDestino)):
+        #Verificando se um dos campos está vazio
+        if isna(listaOrigem[i]) or isna(listaDestino[i]):
+
+            #o pandas faz a leitura mínima das primeiras 8 linhas, a linha vai até a 7 pois tem o cabeçalho
+            #Se for até a linha 8
+            if linha <= 7:
+                if((isna(listaOrigem[i]) == False and isna(listaDestino[i]) == True) or (isna(listaOrigem[i]) == True and isna(listaDestino[i]) == False)):
+                    print(f"Valores vazio na linha {linha}")
+                    print(f"Origem: {listaOrigem[i]}")
+                    print(f"Destino: {listaDestino[i]}")
+                    print('-'*50)
+
+            #Acima da linha 7
+            if linha > 7:
+                print(f"Valores vazio na linha {linha}")
+                print(f"Origem: {listaOrigem[i]}")
+                print(f"Destino: {listaDestino[i]}")
+                print('-'*50)
+
+            linha += 1
+            continue
+
         try:
             #Verificando se a origem é pasta
             if(path.isdir(listaOrigem[i]) == True):
+
                 #Verificando se o destino existe
-                if(path.isdir(listaDestino[i]) == True):
-                    copytree(listaOrigem[i], listaDestino[i])
-                else:
+                if not path.isdir(listaDestino[i]):
+
                     #Criando as pastas não existentes
                     makedirs(listaDestino[i])
-                    copytree(listaOrigem[i], listaDestino[i])
+
+                #Copiando e Colando a pasta
+                copytree(listaOrigem[i], listaDestino[i], dirs_exist_ok=True)
 
             #Verificando se a origem é arquivo
             elif(path.isfile(listaOrigem[i]) == True):
+
                 #Verificando se o Destino existe
-                if(path.isdir(listaDestino[i]) == True):
-                    copy(listaOrigem[i], listaDestino[i])
-                else:
+                if not path.isdir(listaDestino[i]):
+
                     #Criando as pastas não existentes
-                    makedirs(listaDestino[i])
-                    copy(listaOrigem[i], listaDestino[i])
+                    makedirs(listaDestino[i], exist_ok=True)
+
+                #Copiando e Colando o arquivo
+                copy(listaOrigem[i], listaDestino[i])
 
             #Condição para caso a origem não exista
             else:
-                print(f"Origem Inexistente: {listaOrigem[i]}")
+                print(f"Origem Inexistente na linha {linha}")
+                print(f"Origem: {listaOrigem[i]}")
+                print('-' * 50)
+
 
         except Exception as e:
             print(f"Erro na execução Origem: {listaOrigem[i]} e Destino: {listaDestino[i]}")
+            print(f"Provavelmente na linha {linha} do excel")
             print(e)
-            print('-'*30)
+            print('-'*50)
+        linha +=1
 
     input("Finalizado!!!")
-
-
 
 autoTransfer()
 
